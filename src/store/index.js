30544eef9af289516c986a3bd1aef6a5e9a89a47
robiverdev <robi.verdev1@gmail.com>
2025-05-04 20:24:58 +0200

    Added contact form and storing requests with Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index.js';
+import requestModules from './modules/requests/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
+    requests: requestModules,
   },
   state() {
     return {
