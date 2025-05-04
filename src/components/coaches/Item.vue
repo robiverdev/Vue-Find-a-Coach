99bfe5fe712f50ba462976a2df912541693c2735
robiverdev <robi.verdev1@gmail.com>
2025-05-04 13:47:21 +0200

    Added Base components

--- src/components/coaches/Item.vue
+++ src/components/coaches/Item.vue
@@ -3,11 +3,11 @@
     <h3>{{ fullName }}</h3>
     <h4>${{ rate }}/hour</h4>
     <div>
-      <span v-for="area in areas" :key="area">{{ area }}</span>
+      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
     </div>
     <div class="actions">
-      <router-link :to="coachContactLink">Contact</router-link>
-      <router-link :to="coachDetailsLink">Details</router-link>
+      <base-button mode="outline" link :to="coachContactLink">Contact</base-button>
+      <base-button link :to="coachDetailsLink">Details</base-button>
     </div>
   </li>
 </template>
@@ -17,7 +17,7 @@
     props: ['id', 'firstName', 'lastName', 'rate', 'areas'],
     computed: {
       fullName() {
-        return this.firstName + this.lastName;
+        return this.firstName + ' ' + this.lastName;
       },
       coachContactLink() {
         return this.$route.path + '/' + this.id + '/contact'; // /coaches/c1/contact
--- src/pages/coaches/List.vue
+++ src/pages/coaches/List.vue
@@ -1,22 +1,24 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <Item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></Item>
-    </ul>
-    <h3 v-else>No Coaches found</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="/register">Register as a Coach</base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <Item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></Item>
+      </ul>
+      <h3 v-else>No Coaches found</h3>
+    </base-card>
   </section>
 </template>
 
